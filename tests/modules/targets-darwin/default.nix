9e3dc5aeb1218a9ce02d26cc01ed1f79127fe4c1
Mario Rodas <marsam@users.noreply.github.com>
2020-10-26 13:40:46 -0500

    Revert "targets.darwin: disable application directory"
    This reverts commit 41b1af808f1de69c31f3fd164d5274154ee782e4.

--- tests/modules/targets-darwin/default.nix
+++ tests/modules/targets-darwin/default.nix
@@ -1,5 +1,1 @@
-{
-  # Disabled for now due to conflicting behavior with nix-darwin. See
-  # https://github.com/nix-community/home-manager/issues/1341#issuecomment-687286866
-  #targets-darwin = ./darwin.nix;
-}
+{ targets-darwin = ./darwin.nix; }
